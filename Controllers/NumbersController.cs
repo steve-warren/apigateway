cd5d685d3d13ea3e189aec536a2dcb1aec209340
Steve Warren <steve-warren@users.noreply.github.com>
2020-10-28 17:11:13 -0500

    initial

---
+++ Controllers/NumbersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace apigateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumbersController : ControllerBase
+    {
+        private static readonly ConcurrentBag<int> _numbers = new ConcurrentBag<int>(Enumerable.Range(1,10));
+        private readonly ILogger<NumbersController> _logger;
+
+        public NumbersController(ILogger<NumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _logger.LogInformation($"{DateTime.Now} - returning numbers.");
+
+            return Ok(
+                new
+                {
+                    set = _numbers.AsEnumerable(),
+                    time = DateTime.Now
+                });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] int number)
+        {
+            _numbers.Add(number);
+
+            _logger.LogInformation($"{DateTime.Now} - number {number} added to set.");
+
+            return Accepted();
+        }
+    }
+}
